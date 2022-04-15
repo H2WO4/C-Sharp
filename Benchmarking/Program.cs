7240f2ddf59f6f30603de8638918a818187e3987
Arthur <arthurlamidel@gmail.com>
2022-04-15 15:44:10 +0200

    Set up Benchmarking

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarking;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
+
+public enum Test
+{
+    Flag1 = 1 << 0,
+    Flag2 = 1 << 1,
+    Flag3 = 1 << 2,
+}
+
+public class Benchmark
+{
+    
+}
